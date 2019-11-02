cb490e611af01df646b928e4991eb33a7850d75b
bgarcia95 <bgarcia.95@outlook.com>
2019-11-01 21:31:04 -0600

    Adding routing and first components into the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Suscriptores from "./components/suscriptores/Suscriptores";
+import MostrarSuscriptor from "./components/suscriptores/MostrarSuscriptor";
+import NuevoSuscriptor from "./components/suscriptores/NuevoSuscriptor";
+import EditarSuscriptor from "./components/suscriptores/EditarSuscriptor";
 
 function App() {
   return (
-    <h1>Bibliostore</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/suscriptores" component={Suscriptores} />
+        <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+        <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
+        <Route
+          exact
+          path="/suscriptores/editar/:id"
+          component={EditarSuscriptor}
+        />
+      </Switch>
+    </Router>
   );
 }
 
