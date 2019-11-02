bed0c1ad46cd6c1e63a1ca3d90dcab15256806fc
bgarcia95 <bgarcia.95@outlook.com>
2019-11-02 12:43:34 -0600

    Adding routing and components for Libros

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
 
+import Libros from './components/Libros/Libros';
+import MostrarLibro from './components/Libros/MostrarLibro';
+import EditarLibro from './components/Libros/EditarLibro';
+import NuevoLibro from './components/Libros/NuevoLibro';
+import PrestamoLibro from './components/Libros/PrestamoLibro';
+
+
 import Suscriptores from "./components/suscriptores/Suscriptores";
 import MostrarSuscriptor from "./components/suscriptores/MostrarSuscriptor";
 import NuevoSuscriptor from "./components/suscriptores/NuevoSuscriptor";
@@ -17,6 +24,13 @@ function App() {
         <Navbar />
         <div className="container">
           <Switch>
+
+            <Route exact path="/" component={Libros} />
+            <Route exact path="/libros/mostrar/:id" component={MostrarLibro}/>
+            <Route exact path="/libros/nuevo" component={NuevoLibro} />
+            <Route exact path="/libros/editar/:id" component={EditarLibro} />
+            <Route exact path="/libros/prestamo/:id" component={PrestamoLibro} />
+
             <Route exact path="/suscriptores" component={Suscriptores} />
             <Route
               exact
