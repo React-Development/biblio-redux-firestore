fcea7b06b14e5285ad4ef08fa7f184ff8a707ae8
bgarcia95 <bgarcia.95@outlook.com>
2019-11-01 21:42:13 -0600

    Adding navbar into the app

--- src/App.js
+++ src/App.js
@@ -6,19 +6,28 @@ import MostrarSuscriptor from "./components/suscriptores/MostrarSuscriptor";
 import NuevoSuscriptor from "./components/suscriptores/NuevoSuscriptor";
 import EditarSuscriptor from "./components/suscriptores/EditarSuscriptor";
 
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/suscriptores" component={Suscriptores} />
-        <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
-        <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
-        <Route
-          exact
-          path="/suscriptores/editar/:id"
-          component={EditarSuscriptor}
-        />
-      </Switch>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/suscriptores" component={Suscriptores} />
+          <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+          <Route
+            exact
+            path="/suscriptores/mostrar/:id"
+            component={MostrarSuscriptor}
+          />
+          <Route
+            exact
+            path="/suscriptores/editar/:id"
+            component={EditarSuscriptor}
+          />
+        </Switch>
+      </div>
     </Router>
   );
 }
