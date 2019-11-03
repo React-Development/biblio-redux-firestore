4acc2d3aeeb8ee3891698b88fc56dad261c26cde
bgarcia95 <bgarcia.95@outlook.com>
2019-11-02 23:47:12 -0600

    Adding Prestamo Libro interface

--- src/components/Libros/PrestamoLibro.js
+++ src/components/Libros/PrestamoLibro.js
@@ -1,12 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import Spinner from "../layout/Spinner";
+import Swal from "sweetalert2";
 
 class PrestamoLibro extends Component {
-    state = {  }
-    render() { 
-        return (  
-            <h1>Prestamo Libro</h1>
-        );
-    }
+  state = {};
+  render() {
+    // Extraer el libro
+    const { libro } = this.props;
+
+    if (!libro) return <Spinner />;
+
+    return (
+      <div className="row">
+        <div className="col-12 mb-4">
+          <Link to="/" className="btn btn-secondary">
+            <i className="fas fa-arrow-circle-left"></i> {""}
+            {""}
+            Volver al Listado
+          </Link>
+        </div>
+        <div className="col-12">
+          <h2>
+            <i className="fas fa-book"></i> {""}
+            Solicitar Prestamo: {libro.titulo}
+          </h2>
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <form>
+                <legend className="color-primary text-center">
+                  Busca el Suscriptor por Código
+                </legend>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
-export default PrestamoLibro;
+
+PrestamoLibro.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default compose(
+  firestoreConnect(props => [
+    {
+      collection: "libros",
+      storeAs: "libro",
+      doc: props.match.params.id
+    }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    libro: ordered.libro && ordered.libro[0]
+  }))
+)(PrestamoLibro);
